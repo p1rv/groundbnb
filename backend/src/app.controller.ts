fbd37b4087c835a5c869d097da38590e78c24ccd
p1rv <karol.p.krol@gmail.com>
2024-01-24 15:09:47 +0100

    docker migration

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('places')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getAll(): string {
-    return this.appService.getAll();
+  getHello(): { [key: string]: string } {
+    return this.appService.getHello();
   }
 }
